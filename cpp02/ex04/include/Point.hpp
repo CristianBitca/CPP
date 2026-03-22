bdeb3e29a7687d36c9eda31caf66470d0de60894
CristianBitca <cristibitca2020@gmail.com>
2026-03-22 13:10:19 +0000

    cpp04 ex00 and ex01 done, plus start to work on the cpp02 ex03

---
+++ cpp02/ex04/include/Point.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbitca <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 09:13:55 by cbitca            #+#    #+#             */
+/*   Updated: 2026/03/19 13:17:33 by cbitca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef POINT_HPP
+# define POINT_HPP
+
+#include "Fixed.hpp"
+
+class Point
+{
+private:
+    Fixed const x;
+    Fixed const y;
+public:
+    Point();
+    Point(const float x, const float y);
+    Point(const Point& other);
+    Point& operator=(const Point& other);
+    ~Point();
+
+    Fixed getX() const;
+    Fixed getY() const;
+};
+
+#endif
